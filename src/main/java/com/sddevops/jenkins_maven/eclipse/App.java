17eedaec70b63b086e618c23afb3d38ae77aba3d
tp2024sc <tp2024scdevops@gmail.com>
2024-02-13 21:52:50 +0800

    Commit Lily

--- src/main/java/com/sddevops/jenkins_maven/eclipse/App.java
+++ src/main/java/com/sddevops/jenkins_maven/eclipse/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World! To Master (REMOTE)" );
+        System.out.println( "Hello World! To Master Lily" );
     }
 }
