f9356e967aae635d5d44faccaf2894311b65f695
tp2024sc <tp2024scdevops@gmail.com>
2024-02-13 22:50:03 +0800

    try genkins 004

--- src/main/java/com/sddevops/jenkins_maven/eclipse/App.java
+++ src/main/java/com/sddevops/jenkins_maven/eclipse/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World! To Master (REMOTE)" );
         System.out.println( "Hello World! To Master Lily" );
+        System.out.println( "Hello World! To Master Lily2" );
     }
 }
